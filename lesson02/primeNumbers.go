480e65bc0cac569e5bdc1d5f1593a20f9f64e713
ptsypyshev <ptsypyshev@gmail.com>
2022-02-20 19:43:27 +0500

    Task "Prime Numbers" refactored.

--- lesson02/primeNumbers.go
+++ lesson02/primeNumbers.go
@@ -11,32 +11,24 @@ func main() {
 	} else {
 		//Init Slice with true values for Sieve of Eratosthenes
 		//https://ru.wikipedia.org/wiki/%D0%A0%D0%B5%D1%88%D0%B5%D1%82%D0%BE_%D0%AD%D1%80%D0%B0%D1%82%D0%BE%D1%81%D1%84%D0%B5%D0%BD%D0%B0
-		initSlice := make([]bool, stopNumber)
+		initSlice := make([]bool, stopNumber+1)
 
 		// Start Searching Prime Numbers
-		var result []int
 		step := 2
-		for step < stopNumber {
-			result = append(result, step)
-			for j := 2 * step; j < stopNumber; j += step {
-				initSlice[j] = true // cross a composite number off the slice
-			}
-			nextStep := step
-			for idx, val := range initSlice[step+1:] {
-				// Check the next true element in slice
-				if !val {
-					nextStep += idx + 1
-					break
+		for step <= stopNumber {
+			if !initSlice[step] {
+				for j := 2 * step; j <= stopNumber; j += step {
+					initSlice[j] = true // cross a composite number off the slice
 				}
 			}
+			step += 1
+		}
 
-			// Break the loop if no next step found
-			if nextStep == step {
-				break
-			} else {
-				step = nextStep
+		fmt.Print("Prime Numbers are ")
+		for idx, val := range initSlice[2:] {
+			if !val {
+				fmt.Printf("%v ", idx+2)
 			}
 		}
-		fmt.Println("Prime Numbers are", result)
 	}
 }
