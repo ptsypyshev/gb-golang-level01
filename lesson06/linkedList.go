e470f7cac10f8a2d37731a6d90c26a3f8eef34ac
ptsypyshev <ptsypyshev@gmail.com>
2022-03-09 13:51:05 +0500

    Task "LinkedList" is refactored.

--- lesson06/linkedList.go
+++ lesson06/linkedList.go
@@ -22,30 +22,35 @@ func (list *SingleList) isEmpty() bool {
 	return list.length == 0
 }
 
-func (list *SingleList) addFirst(newVal interface{}) {
+func (list *SingleList) adder(newVal interface{}, operation string, n int) {
 	node := &Node{val: newVal}
-	if list.isEmpty() {
+	switch {
+	case list.isEmpty():
 		list.head = node
-	} else {
+	case operation == "addFirst":
 		node.next, list.head = list.head, node
-	}
-	list.length++
-	return
-}
-
-func (list *SingleList) addLast(newVal interface{}) {
-	node := &Node{val: newVal}
-	if list.isEmpty() {
-		list.head = node
-	} else {
+	case operation == "addLast":
 		step := list.head
 		for step.next != nil {
 			step = step.next
 		}
 		step.next = node
+	case operation == "addMiddle":
+		step := list.head
+		for i := 0; i < n; i++ {
+			step = step.next
+		}
+		step.next, node.next = node, step.next
 	}
 	list.length++
-	return
+}
+
+func (list *SingleList) addFirst(newVal interface{}) {
+	list.adder(newVal, "addFirst", 0)
+}
+
+func (list *SingleList) addLast(newVal interface{}) {
+	list.adder(newVal, "addLast", 0)
 }
 
 func (list *SingleList) addMiddle(newVal interface{}, n int) error {
@@ -55,18 +60,7 @@ func (list *SingleList) addMiddle(newVal interface{}, n int) error {
 	if n > list.length {
 		return fmt.Errorf("you cannot insert %v after %d because original list is too small", newVal, n)
 	}
-	node := &Node{val: newVal}
-
-	if list.isEmpty() {
-		list.head = node
-	} else {
-		step := list.head
-		for i := 0; i < n; i++ {
-			step = step.next
-		}
-		step.next, node.next = node, step.next
-	}
-	list.length++
+	list.adder(newVal, "addMiddle", n)
 	return nil
 }
 
@@ -77,9 +71,10 @@ func (list *SingleList) getHead() (interface{}, error) {
 	return list.head.val, nil
 }
 
-func (list *SingleList) printList() error {
+func (list *SingleList) printList() {
 	if list.isEmpty() {
-		return fmt.Errorf("single List is empty")
+		fmt.Println("Single List is empty")
+		return
 	}
 	fmt.Printf("{ ")
 	step := list.head
@@ -88,11 +83,24 @@ func (list *SingleList) printList() error {
 		step = step.next
 	}
 	fmt.Printf("}\n")
-	return nil
+}
+
+func (list *SingleList) reverseList() {
+	step := list.head
+	var prev *Node
+
+	for step != nil {
+		next := step.next
+		step.next = prev
+		prev = step
+		step = next
+	}
+	list.head = prev
 }
 
 func main() {
 	llist := initSingleList()
+	llist.printList()
 	fmt.Printf("Is your list empty? %t\n", llist.isEmpty())
 	llist.addLast("last")
 	fmt.Printf("Is your list empty? %t\n", llist.isEmpty())
@@ -111,5 +119,8 @@ func main() {
 		return
 	}
 	fmt.Println(res)
+	llist.printList()
+
+	llist.reverseList()
 	llist.printList()
 }
